22e82b1311d0f3eb5ca0e40025284b5e7babaa7d
PiotrDziedzic <31729628+PiotrDziedzic@users.noreply.github.com>
2026-04-06 09:29:45 +0200

    Add basic App

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 20, fontFamily: 'Arial' }}>
+      <h1>Erebus</h1>
+      <p>Backend and Frontend are running</p>
+    </div>
+  )
+}
